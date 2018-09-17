b3ccbe83888cff53344f068bb58077cde4f982d2
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:15:27 +0900

    drill-05 def 'left move"  definition to commit
    drill-05 def 'left move"  definition to commit

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -6,6 +6,13 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 def move_form_center_to_right():
+    x, y = 800 // 2, 90
+    while x < 800 - 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        x += 2
+        delay(0.01)
     pass
 def move_up():
     pass
