299789dae11428f6e96047cf4f8c0114087e05e4
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:44:56 +0900

    drill-05 def "make_circle" definition
    drill-05 def "make_circle" definition

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -1,5 +1,4 @@
 from pico2d import *
-
 open_canvas()
 
 grass = load_image('grass.png')
@@ -52,12 +51,24 @@ def make_rectangle():
     #move_up()
     #move_left()
     #move_down()
-    move_from_left_to_center()
+    #move_from_left_to_center()
 
+    import math
 def make_circle():
+    cx, cy, r = 800 // 2, 600 // 2, (600-180) // 2
+    degree = - 90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
+        delay(0.01)
     pass
 while True:
-    make_rectangle()
+    #make_rectangle()
     make_circle()
 
 close_canvas()
