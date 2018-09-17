321e90c654caa8c0d2420b6223072a2896090a28
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:29:17 +0900

    drill-05 def "move_down" definition
    drill-05 def "move_down" definition

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -31,14 +31,20 @@ def move_left():
         x -= 2
         delay(0.01)
 def move_down():
-    pass
+    x, y = 0 + 25, 600 - 50
+    while y > 30 + 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
 def move_from_left_to_center():
     pass
 
 def make_rectangle():
     #move_from_center_to_right()
     #move_up()
-    move_left()
+    #move_left()
     move_down()
     move_from_left_to_center()
 
