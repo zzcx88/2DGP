432f4a501871c9800ab28591545055421e21660d
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:04:53 +0900

    drill-05 commit finished
    code finished

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -5,5 +5,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+while True:
+    make_rectangle()
+    make_circle()
 
 close_canvas()
