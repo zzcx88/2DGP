b212f6385c4b3f8f14891254319b6c74be2e53ea
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:21:36 +0900

    drill-05 "def move_up" definition
    drill-05 "def move_up" definition

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -13,22 +13,28 @@ def move_form_center_to_right():
         character.draw_now(x,y)
         x += 2
         delay(0.01)
-    pass
 def move_up():
-    pass
+    x, y = 800 - 25, 90
+    while y < 600 -50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+
 def move_left():
     pass
 def move_down():
     pass
-def move_from_left_to_cnetert():
+def move_from_left_to_center():
     pass
 
 def make_rectangle():
-    move_form_center_to_right()
+    #move_from_center_to_right()
     move_up()
     move_left()
     move_down()
-    move_from_left_to_cnetert()
+    move_from_left_to_center()
 
 def make_circle():
     pass
