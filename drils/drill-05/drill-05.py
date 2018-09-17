9ab0f2e4f84cc01eb5572c0825dd4ff7e3e362fe
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 19:59:31 +0900

    ***drill-05 general logic bug fixed***
    ***drill-05 general logic bug fixed***
    -the character move x,y cordinate err fixed
    -for coordinate check more deliberatly with use 'print(x,y)'

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -8,6 +8,8 @@ def move_to_132_243():
     # character's defaultcordinate
     x = 800 // 2
     y = 0 + 60
+    xb = x - 132
+    yb = 243 - y
     frame = 0
     bottom = 0
     while x > 132 and y < 243:
@@ -17,12 +19,14 @@ def move_to_132_243():
         frame = (frame + 1) % 8
         update_canvas()
         x -= 2
-        y += 2
-        delay(0.05)
-
+        y += yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_435_470():
     x = 132
     y = 243
+    xb = 435 - x
+    yb = 475 - y
     frame = 0
     bottom = 100
     while x < 435 and y < 470:
@@ -32,11 +36,14 @@ def move_to_435_470():
         frame = (frame + 1) % 8
         update_canvas()
         x += 2
-        y += 2
-        delay(0.05)
+        y += yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_477_203():
     x = 435
     y = 470
+    xb = 477 - x
+    yb = y - 203
     frame = 0
     bottom = 100
     while x < 477 and y > 203:
@@ -46,11 +53,14 @@ def move_to_477_203():
         frame = (frame + 1) % 8
         update_canvas()
         x += 2
-        y -= 2
-        delay(0.05)
+        y -= yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_715_136():
     x = 477
     y = 203
+    xb = 714 - x
+    yb = y - 136
     frame = 0
     bottom = 100
     while x < 715 and y > 136:
@@ -60,11 +70,14 @@ def move_to_715_136():
         frame = (frame + 1) % 8
         update_canvas()
         x += 2
-        y -= 2
-        delay(0.05)
+        y -= yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_316_225():
     x = 715
     y = 136
+    xb = x - 316
+    yb = 225 - y
     frame = 0
     bottom = 0
     while x > 316 and y < 225:
@@ -74,25 +87,56 @@ def move_to_316_225():
         frame = (frame + 1) % 8
         update_canvas()
         x -= 2
-        y += 2
-        delay(0.05)
+        y += yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_510_92():
-    pass
+    x = 316
+    y = 225
+    xb = 510 - x
+    yb = y - 92
+    frame = 0
+    bottom = 100
+    while x < 510 and y > 92:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, bottom, 100, 100, x, y)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x += 2
+        y -= yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_692_518():
-    pass
+    x = 510
+    y = 92
+    xb = 692 - x
+    yb = 518 - y
+    frame = 0
+    bottom = 0
+    while x > 316 and y < 225:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, bottom, 100, 100, x, y)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x -= 2
+        y += yb / (xb / 2)
+        delay(0.01)
+        print(x, y)
 def move_to_682_336():
     pass
 def move_to_712_349():
     pass
 
 def move_to_cordinate():
-    #move_to_132_243()
-    #move_to_435_470()
-    #move_to_477_203()
-    #move_to_715_136()
+    move_to_132_243()
+    move_to_435_470()
+    move_to_477_203()
+    move_to_715_136()
     move_to_316_225()
-    #move_to_510_92()
-    #move_to_692_518()
+    move_to_510_92()
+    move_to_692_518()
     #move_to_682_336()
     #move_to_712_349()
 while True:
