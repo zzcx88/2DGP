bec65d707be96a779d1bb1c77b25211dba4fc157
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:31:57 +0900

    drill-05 def " move_from_left_to_cente" definition
    drill-05 def " move_from_left_to_cente" definition

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -39,13 +39,19 @@ def move_down():
         y -= 2
         delay(0.01)
 def move_from_left_to_center():
-    pass
+    x, y = 0 + 25, 30 + 50
+    while x < 800 // 2:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
 
 def make_rectangle():
     #move_from_center_to_right()
     #move_up()
     #move_left()
-    move_down()
+    #move_down()
     move_from_left_to_center()
 
 def make_circle():
