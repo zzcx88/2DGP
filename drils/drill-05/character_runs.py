154c0041144f1f3df797c449b63313e1484242a9
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 17:08:04 +0900

    drill-05 make def architected
    drill-05 make def architected

--- drils/drill-05/character_runs.py
+++ drils/drill-05/character_runs.py
@@ -1,15 +1,15 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('run_animation.png')
 
 x = 0
 frame = 0
 while (x < 800):
     clear_canvas()
     grass.draw(400, 30)
-    #character.clip_draw(frame * 10, 0, 100, 100, x, 90)
-    character.draw(x, 90)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    #character.draw(x, 90)
     update_canvas()
     frame = (frame + 1) % 8
     x += 5
--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -4,71 +4,40 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def  move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x,y)
-        x += 2
-        delay(0.01)
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 -50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
-
-def move_left():
-    x, y = 800 - 25, 600 - 50
-    while x > 0 + 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-def move_down():
-    x, y = 0 + 25, 600 - 50
-    while y > 30 + 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-def move_from_left_to_center():
-    x, y = 0 + 25, 30 + 50
-    while x < 800 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
-
-    import math
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600-180) // 2
-    degree = - 90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
+def change_direction():
     pass
+
+
+def move_to_132_243():
+    pass
+def move_to_435_470():
+    pass
+def move_to_477_203():
+    pass
+def move_to_715_136():
+    pass
+def move_to_316_225():
+    pass
+def move_to_510_92():
+    pass
+def move_to_692_518():
+    pass
+def move_to_682_336():
+    pass
+def move_to_712_349():
+    pass
+
+def move_to_cordinate():
+    move_to_132_243()
+    move_to_435_470()
+    move_to_477_203()
+    move_to_715_136()
+    move_to_316_225()
+    move_to_510_92()
+    move_to_692_518()
+    move_to_682_336()
+    move_to_712_349()
 while True:
-    make_rectangle()
-    make_circle()
+    move_to_cordinate()
 
 close_canvas()
