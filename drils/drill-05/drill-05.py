118e658aa8dc95e39796191967d373a38e477054
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 17:28:51 +0900

    drill-05 def "move_to_132_243 definition
    drill-05 def "move_to_132_243 definition

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -2,14 +2,24 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
-
-def change_direction():
-    pass
-
+character = load_image('animation_sheet.png')
 
 def move_to_132_243():
-    pass
+    # character's defaultcordinate
+    x = 800 // 2
+    y = 0 + 60
+    frame = 0
+    bottom = 0
+    while x > 132 and y < 243:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, bottom, 100, 100, x, y)
+        frame = (frame + 1) % 8
+        update_canvas()
+        x -= 2
+        y += 2
+        delay(0.05)
+
 def move_to_435_470():
     pass
 def move_to_477_203():
@@ -29,14 +39,14 @@ def move_to_712_349():
 
 def move_to_cordinate():
     move_to_132_243()
-    move_to_435_470()
-    move_to_477_203()
-    move_to_715_136()
-    move_to_316_225()
-    move_to_510_92()
-    move_to_692_518()
-    move_to_682_336()
-    move_to_712_349()
+    #move_to_435_470()
+    #move_to_477_203()
+    #move_to_715_136()
+    #move_to_316_225()
+    #move_to_510_92()
+    #move_to_692_518()
+    #move_to_682_336()
+    #move_to_712_349()
 while True:
     move_to_cordinate()
 
