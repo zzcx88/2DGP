d4b7b8be5f9246aa947e625fe398f2452845561f
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-17 16:09:57 +0900

    drill-05 def definition to commit
    drill-05 def definition to commit

--- drils/drill-05/drill-05.py
+++ drils/drill-05/drill-05.py
@@ -5,8 +5,23 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def make_rectangle():
+def move_form_center_to_right():
     pass
+def move_up():
+    pass
+def move_left():
+    pass
+def move_down():
+    pass
+def move_from_left_to_cnetert():
+    pass
+
+def make_rectangle():
+    move_form_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_left_to_cnetert()
 
 def make_circle():
     pass
