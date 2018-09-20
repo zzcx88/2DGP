06cefe2ab9ea0f3de13d4d8c122a9c31f636b7a0
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-09-20 17:19:00 +0900

    drill-06 moving
    클릭시 움직임 구현

--- drils/drill-06/drill-06.py
+++ drils/drill-06/drill-06.py
@@ -6,12 +6,14 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
    global running
    global x, y
+   global ifClick
    events = get_events()
    for event in events:
         if event.type == SDL_QUIT:
            running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
            x , y = event.x, KPU_HEIGHT - 1 - event.y
+           ifClick = True
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
            running = False
 
@@ -25,23 +27,36 @@ mouse_cursor = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+ifClick = True
 cx = 0
 cy = 0
 frame = 0
 ## hide_cursor()
-character.clip_draw(frame * 100, 100 * 1, 100, 100, KPU_WIDTH / 2 , KPU_HEIGHT / 2)
+#character.clip_draw(frame * 100, 100 * 1, 100, 100, KPU_WIDTH / 2 , KPU_HEIGHT / 2)
 while running:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    while cx < x and cy < y:
-        cx += 5
-        cy += 5
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
-    update_canvas()
-    frame = (frame + 1) % 8
-
-    delay(0.02)
-    handle_events()
+    while ifClick == True:
+        while x != cx and y != cy:
+            mouse_cursor.draw(100, 100)
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            if x - cx < 0 and y - cy < 0:
+                cx -= 5
+                cy -= y / (x / 5)
+            if x - cx > 0 and y - cy > 0:
+                cx += 5
+                cy += y / (x / 5);
+            if x - cx < 0 and y - cy > 0:
+                cx -= 5
+                cy += y / (x / 5);
+            if x - cx > 0 and y - cy < 0:
+                cx += 5
+                cy -= y / (x / 5);
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
+            frame = (frame + 1) % 8
+            update_canvas()
+            delay(0.02)
+            handle_events()
+    ifClick = False
 
 close_canvas()
 
