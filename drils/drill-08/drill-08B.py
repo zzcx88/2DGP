b2dc1967660fb02947c497590fa305a575c8c5e9
zzcx88(gs40-6qe) <zzcx88@naver.com>
2018-10-04 17:18:58 +0900

    drill-08B fail
    drill-08B fail

--- drils/drill-08/drill-08B.py
+++ drils/drill-08/drill-08B.py
@@ -8,19 +8,17 @@ character = load_image('animation_sheet.png')
 frame = 0
 size = 10
 points = [(random.randint(300, 600), random.randint(300, 600)) for i in range(size)]
-n = 3
-
+n = 0
 def draw_curve(p1, p2, p3):
     pass
 
 while True:
-    x, y = points[n-1]
-    tx, ty = points[n]
-    clear_canvas()
     i = 0
+    x, y = points[n - 1]
+    tx, ty = points[n]
     while tx != x and y != ty:
-        print(points)
         clear_canvas()
+        print((x, y), (tx, ty))
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * points[n-3][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * points[n-2][0] + (-3 * t ** 3 + 4 * t ** 2 + t) * points[n-1][0] + (t ** 3 - t ** 2) * points[n][0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * points[n-3][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * points[n-2][1] + (-3 * t ** 3 + 4 * t ** 2 + t) * points[n-1][1] + (t ** 3 - t ** 2) * points[n][1]) / 2
